1895f98bf29d76dc442015e027028c21c76e9cc6
Jihoon Oh <jihoon.oh@edu.sait.ca>
2022-04-19 00:15:36 -0600

    add CoinEat class

---
+++ src/CoinEat/CoinEat.java
@@ -0,0 +1,11 @@
+package CoinEat;
+
+import javax.swing.*;
+
+public class CoinEat  extends JFrame {
+    public CoinEat(){
+
+    }
+
+
+}
---
+++ src/CoinEat/Main.java
@@ -0,0 +1,9 @@
+package CoinEat;
+
+import javax.swing.*;
+
+public class Main  {
+    public static void main(String[] args) {
+
+    }
+}
